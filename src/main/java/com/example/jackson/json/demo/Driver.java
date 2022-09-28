d3113ec42d3d5274bd82b7de6454a830065e2c00
Chowdhury Mohammad Irfanuddin <irfan.muhit85@gmail.com>
2022-09-28 10:11:38 +0600

    Reading data from complex JSON file

--- src/main/java/com/example/jackson/json/demo/Driver.java
+++ src/main/java/com/example/jackson/json/demo/Driver.java
@@ -11,11 +11,20 @@ public class Driver {
 			ObjectMapper mapper = new ObjectMapper();
 			
 			// Convert JSON file to Java POJO
-			Student student = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+			Student student = mapper.readValue(new File("data/sample-full.json"), Student.class);
 			
 			// Print data
 			System.out.println("First Name: " + student.getFirstName());
 			System.out.println("Last Name: " + student.getLastName());
+			
+			Address address = student.getAddress();
+			
+			System.out.println("Street: " + address.getStreet());
+			System.out.println("City: " + address.getCity());
+			
+			for(String language : student.getLanguages()) {
+				System.out.println(language);
+			}
 			
 		} catch(Exception e) {
 			e.printStackTrace();
